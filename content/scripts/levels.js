643d32ff2d25e40f437806aec9d7e19ec1b649a9
Ionut Blanaru <blanaru.ionut@gmail.com>
2024-08-06 10:48:09 +0000

    Trying to fix issue with GitHub pages internal links not working

--- content/scripts/levels.js
+++ content/scripts/levels.js
@@ -8,7 +8,7 @@ function levelOne(){
     document.querySelector(".start-game").addEventListener('click', function(){
 
         document.querySelector(".button-wrapper").style.display = "none"
-        document.querySelector(".menu-container").style["background-image"] = 'url("./content/img/level1-background.png")'
+        document.querySelector(".menu-container").style["background-image"] = 'url("content/img/level1-background.png")'
         document.querySelector(".title-heading").innerHTML = 'Level 1';
         document.querySelector(".game-music-btn").style.display = "block";
         document.querySelector(".score").style.display = "block";
@@ -32,7 +32,7 @@ function levelOneBoard(){
     
     for ( let i = 0; i < 12; i++ ) {
        const card = document.createElement("img")
-        card.setAttribute('src', "/content/img/card-back.png");
+        card.setAttribute('src', "content/img/card-back.png");
         card.setAttribute('data-id', i)
         card.addEventListener('click', flipCard)
         gameGrid.appendChild(card);
@@ -48,19 +48,19 @@ function levelOneBoard(){
         
        if (optionOneid == optionTwoid){
             successMatchAudio();
-            cards[optionOneid].setAttribute('src', "./content/img/empty-card.png");
-            cards[optionTwoid].setAttribute('src', "./content/img/empty-card.png");
+            cards[optionOneid].setAttribute('src', "content/img/empty-card.png");
+            cards[optionTwoid].setAttribute('src', "content/img/empty-card.png");
        }    
        else if (cardsChosen[0] === cardsChosen[1]) {
             successMatchAudio();
-            cards[optionOneid].setAttribute('src', "./content/img/empty-card.png");
-            cards[optionTwoid].setAttribute('src', "./content/img/empty-card.png");
+            cards[optionOneid].setAttribute('src', "content/img/empty-card.png");
+            cards[optionTwoid].setAttribute('src', "content/img/empty-card.png");
             cards[optionOneid].removeEventListener('click',flipCard);
             cards[optionTwoid].removeEventListener('click',flipCard);
             cardsWon.push(cardsChosen);
         } else {
-            cards[optionOneid].setAttribute('src', "./content/img/card-back.png");
-            cards[optionTwoid].setAttribute('src', "./content/img/card-back.png");
+            cards[optionOneid].setAttribute('src', "content/img/card-back.png");
+            cards[optionTwoid].setAttribute('src', "content/img/card-back.png");
             failedMatchAudio();
         }
 
